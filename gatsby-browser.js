9a516820783fe0700b27418bab9c058d359dccab
Jessica Neary <jessicafneary@gmail.com>
2022-03-04 17:52:34 +1300

    Added necessary checkout details

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { CartProvider } from "use-shopping-cart"
 
+import 'bootstrap/dist/css/bootstrap.css';
+import './src/styles.css';
+
 const wrapPageElement = ({ element }) => {
     return (
     <CartProvider
         mode="payment"
-        cartMode="checkout-session"
+        cartMode="client-only"
         stripe={process.env.STRIPE_PUBLISHABLE_KEY}
+        successUrl={`${window.location.origin}/payment-success/`}
+        cancelUrl={`${window.location.origin}/`}
+        billingAddressCollection={true}
+        currency="NZD"
     >
         {element}
     </CartProvider>
