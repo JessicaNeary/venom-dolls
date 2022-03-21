b5ca428aef75638c99bd49764fe5432f767df810
Jessica Neary <jessicafneary@gmail.com>
2022-03-22 01:52:38 +1300

    Adjusted email template

--- amplify/backend/function/sendEmail/src/index.js
+++ amplify/backend/function/sendEmail/src/index.js
@@ -11,7 +11,7 @@ exports.handler = async (event) => {
           ToAddresses: ["jessicafneary@gmail.com"],
         },
         Template: 'PurchaseNotificationEmail',
-        TemplateData: `{ \"name\":\"${event.shipping.name}\", \"line1\":\"${event.shipping.address.line1}\", \"line2\":\"${event.shipping.address.line2}\", \"city\":\"${event.shipping.address.city}\", \"postal_code\":\"${event.shipping.address.postal_code}\", \"country\":\"${event.shipping.address.country}\", \"customerEmail\": \"${event.customer_details}\", \"totalPrice\": \"${formatPrice(event.amount_total)}\", \"shippingCost\": \"${formatPrice(event.total_details.amount_shipping)}\", \"tax\": \"${formatPrice(event.total_details.amount_tax)}\", \"subtotal\": \"${formatPrice(event.amount_subtotal)}\", \"products_purchased\": \"${event.metadata.products_purchased}\", \"stripeUrl\":\"www.stripe.com\"}`,
+        TemplateData: `{ \"name\":\"${event.shipping.name}\", \"line1\":\"${event.shipping.address.line1}\", \"line2\":\"${event.shipping.address.line2}\", \"city\":\"${event.shipping.address.city}\", \"postal_code\":\"${event.shipping.address.postal_code}\", \"country\":\"${event.shipping.address.country}\", \"customerEmail\": \"${event.customer_details.email}\", \"totalPrice\": \"${formatPrice(event.amount_total)}\", \"shippingCost\": \"${formatPrice(event.total_details.amount_shipping)}\", \"tax\": \"${formatPrice(event.total_details.amount_tax)}\", \"subtotal\": \"${formatPrice(event.amount_subtotal)}\", \"products_purchased\": \"${event.metadata.products_purchased}\", \"stripeUrl\":\"www.stripe.com\"}`,
         Source: "jessicafneary@gmail.com",
         ConfigurationSetName: "EmailConfigSet"
       };
