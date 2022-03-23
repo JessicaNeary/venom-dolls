77bd54c7acf08aec0e09d45e941a78c712fe3ee2
Jessica Neary <jessicafneary@gmail.com>
2022-03-23 23:29:29 +1300

    Styled event page

--- src/pages/events.js
+++ src/pages/events.js
@@ -1,6 +1,9 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import Card from "react-bootstrap/Card";
 
+import formatDate from "../utils/formatDate";
+import formatLocation from "../utils/formatLocation";
 import { getEvents } from "../actions";
 import Layout from "../components/Layout";
 
@@ -16,23 +19,30 @@ function Events({ location }) {
 
     return (
       <Layout path={location.pathname}>
-        <div className="pt-4 text-white d-flex flex-column justify-content-center align-items-center bg-dark w-100">
-          <h2 className="py-4">UPCOMING EVENTS</h2>
+        <div className="pt-4 text-light d-flex flex-column justify-content-center align-items-center bg-dark w-100">
+          <h2 className="pt-4 pb-2 my-4">UPCOMING EVENTS</h2>
           {(() => {
             if (events.current[0]) {
-              return events.current.map(event => <EventItem event={event} />)
+              return (
+                <div className="row justify-content-center">
+                  {events.current.map(event => <EventItem event={event} />)}
+                </div>)
             } else if (eventsLoading) {
               return (
                 <div>Loading</div>
               )
             } else return (
-              <div className="mb-4">There are currently no upcoming events, but gigs are added regularly, so follow our facebook page to keep updated!</div>
+              <div className="mb-4 pb-4 w-75 text-center">We currently don't have any events available to see online, but there are always plenty in the works, so follow our <a target="_blank" rel="noreferrer" href="https://www.facebook.com/thevenomdolls/">facebook page</a> to keep updated!</div>
             )
           })()}
-          <h2 className="py-4">PAST EVENTS</h2>
+          <h2 className="pt-4 pb-2 my-4">PAST EVENTS</h2>
           {(() => {
             if (events.past[0]) {
-              return events.past.map(event => <EventItem event={event} />)
+              return (
+                <div className="row justify-content-center">
+                {events.past.map(event => <EventItem event={event} isPast />)}
+                </div>
+              )
             } else if (eventsLoading) {
               return (
                 <div>Loading</div>
@@ -47,10 +57,24 @@ function Events({ location }) {
     );
   }
 
-  const EventItem = ({ event }) => (
-    <div className="mb-4" key={event.id}>
-      {event.name}
-    </div>
+  const EventItem = ({ event }, isPast) => (
+    <Card className="mb-4 p-0 text-dark col-10 col-md-7" key={event.id}>
+      <Card.Img variant="top" src={event.cover.source} alt={event.name} />
+      <Card.Body>
+        <Card.Title className="fw-bold">{event.name}</Card.Title>
+        <Card.Subtitle className="text-secondary fw-bold">{formatDate(event.start_time)}</Card.Subtitle>
+          <Card.Subtitle className="pt-4 text-primary">
+          { event.place.location ?
+          <a target="_blank" rel="noreferrer" href={`https://www.google.com/maps/search/?api=1&query=${formatLocation(event.place.location)}`}>
+            {event.place.name}, {event.place.location.city}
+          </a> : event.place.name
+          }
+        </Card.Subtitle>
+        <div className="text-center mt-4">
+        <a target="_black" rel="noreferrer" className={`my-2 btn btn-lg btn-outline-${!isPast ? 'primary' : 'secondary disabled'}`} href={`https://www.facebook.com/events/${event.id}`}>Buy Tickets</a>
+        </div>
+      </Card.Body>
+    </Card>
   );
 
   
---
+++ src/utils/formatLocation.js
@@ -0,0 +1,7 @@
+/* generates URI from object containing location info */
+const formatLocation = location => {
+    const string = location.street + ',' + location.city + ',' + location.country + ',' + location.postcode; 
+    return encodeURIComponent(string)
+}
+
+export default formatLocation;
