c46329b837406b712a22579c270ebee2a6dba532
Jessica Neary <jessicafneary@gmail.com>
2022-05-04 21:34:31 +1200

    Updates

--- src/utils/eventInPast.js
+++
@@ -1,9 +0,0 @@
-const eventInPast = (event) => {
-    const currentTime = Date.now();
-    if (event.end_time) {
-        return currentTime > Date.parse(event.end_time);
-    }
-    else return currentTime > Date.parse(event.start_time) + 10800000;
-};
-
-export default eventInPast;
--- src/utils/formatLocation.js
+++
@@ -1,7 +0,0 @@
-/* generates URI from object containing location info */
-const formatLocation = location => {
-    const string = location.street + ',' + location.city + ',' + location.country + ',' + location.postcode; 
-    return encodeURIComponent(string)
-}
-
-export default formatLocation;
