124f2075c4123966cc73e3c3179cf5c45f6c0e14
Jessica Neary <jessicafneary@gmail.com>
2022-05-10 00:09:24 +1200

    Styled 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,54 +1,17 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import React from 'react';
+import Layout from '../components/Layout';
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-
-// markup
-const NotFoundPage = () => {
+function PaymentSuccess({ location }) {
   return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
-  )
+    <Layout path={location.pathname} pageStyles="bg-black">
+      <div className="text-center pt-4 text-white bg-black">
+        <h4 className="py-4 my-4">
+          Page not found
+        </h4>
+        <h6 className="w-75 w-md-50 m-auto">This is not the page you are looking for. You can go about your business <a href="/" className="text-danger text-decoration-none">here</a>.</h6>
+      </div>
+    </Layout>
+  );
 }
 
-export default NotFoundPage
+export default PaymentSuccess;
