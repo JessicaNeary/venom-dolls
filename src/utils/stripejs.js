4b00d75c1c91d9fe0127971f3d3a7428094e0893
Jessica Neary <jessicafneary@gmail.com>
2022-05-17 18:21:47 +1200

    Revert "Hardcoded publishable key"
    This reverts commit db5e3cff9e2fb9a068b141af53b4d7b8a065fc2d.

--- src/utils/stripejs.js
+++ src/utils/stripejs.js
@@ -3,9 +3,7 @@ import { loadStripe } from "@stripe/stripe-js";
 let stripePromise;
 const getStripe = () => {
   if (!stripePromise) {
-    stripePromise = loadStripe(
-      "pk_test_51KSsb9KtGQ4ZHKavaUdMdbZwgsjNGTjPvuCxYJN0vBGvjQBix4P59RlP3SqeEK3f5yNPGDGJWZlv4HinwSq2Nh4r005rU3QLqp"
-    );
+    stripePromise = loadStripe(process.env.GATSBY_STRIPE_PUBLISHABLE_KEY);
   }
   return stripePromise;
 };
