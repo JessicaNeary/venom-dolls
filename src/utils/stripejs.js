db5e3cff9e2fb9a068b141af53b4d7b8a065fc2d
Jessica Neary <jessicafneary@gmail.com>
2022-05-17 16:31:37 +1200

    Hardcoded publishable key

--- src/utils/stripejs.js
+++ src/utils/stripejs.js
@@ -3,7 +3,9 @@ import { loadStripe } from "@stripe/stripe-js";
 let stripePromise;
 const getStripe = () => {
   if (!stripePromise) {
-    stripePromise = loadStripe(process.env.GATSBY_STRIPE_PUBLISHABLE_KEY);
+    stripePromise = loadStripe(
+      "pk_test_51KSsb9KtGQ4ZHKavaUdMdbZwgsjNGTjPvuCxYJN0vBGvjQBix4P59RlP3SqeEK3f5yNPGDGJWZlv4HinwSq2Nh4r005rU3QLqp"
+    );
   }
   return stripePromise;
 };
